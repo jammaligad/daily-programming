fe0d4801c8cd11fac9c9f905d087b92b11a36b74
Jam Maligad <jdmaligad@gmail.com>
2019-01-31 17:48:59 +0800

    unfinished

---
+++ daily-4.py
@@ -0,0 +1,18 @@
+'''
+Given an array of integers, find the first missing positive integer in linear time and constant space. In other words, find the lowest positive integer that does not exist in the array. The array can contain duplicates and negative numbers as well.
+For example, the input [3, 4, -1, 1] should give 2. The input [1, 2, 0] should give 3.
+You can modify the input array in-place.
+'''
+
+values = [int(num) for num in input().split()]
+values.sort()
+
+for index, num in enumerate(values):
+    if values[index+1] < len(values):
+        if num+1 == values[index+1] or num == values[index+1]:
+            pass
+        else:
+            missing = num+1
+    else:
+        pass
+print(missing)
